46f212beabe9be45a9758d9155fb9c0b6a2062c1
Lex Childs <lex148@gmail.com>
2011-07-02 12:08:32 -0500

    wireup of express

--- server.js
+++ server.js
@@ -6,40 +6,76 @@ var exec = require('child_process').exec;
 var qs = require('querystring');
 
 function filename(){
-  return "temp_" + Math.floor(Math.random()*9999999) + ".mp3";
+  return "" + Math.floor(Math.random()*9999999) + ".mp3";
 }
 
 
-var server = http.createServer(
-  function (request, response) {
-  if (request.method == 'POST') {
-    var body = '';
-    var file = filename();
-    request.on('data', function (data) {
-      body += data;
-    });
-    request.on('end', function () {
-      var POST = qs.parse(body);
-      var callback = function(err,stdout, stderr)
-      {
-        sys.puts(stdout); 
-        response.writeHead(200, {'Content-Type': 'audio/mpeg'});
-        response.write(fs.readFileSync(file));
-        response.end();
-        exec("rm " + file, function(){});
-      };
-      exec("echo 'this is a test' | text2wave | lame -V 1 - " + file, callback);
-    });
-  }
-  else {
-    request.on('end', function () {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(fs.readFileSync('index.html'));
+var app = require('express').createServer();
+
+app.get('/', function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(fs.readFileSync('index.html'));
+  res.end();
+});
+
+
+app.post('/read', function(request, response){
+  var body = '';
+  var file = filename();
+  request.on('data', function (data) {
+    body += data;
+  });
+  request.on('end', function () {
+    var POST = qs.parse(body);
+    var callback = function(err,stdout, stderr) {
+      response.write("/audio/" + file);
       response.end();
-    });
-  }
-}
-).listen(1337);
+    };
+    exec("echo 'this is a test' | text2wave | lame -V 1 - audio/" + file, callback);
+  });
+});
+
+
+app.get('/media/*', function(req, res){
+  console.log(req);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  //res.write(fs.readFileSync('index.html'));
+  res.write('test');
+  res.end();
+});
+
+app.listen(1337);
+
+//var server = http.createServer(
+//  function (request, response) {
+//  if (request.method == 'POST') {
+//    var body = '';
+//    var file = filename();
+//    request.on('data', function (data) {
+//      body += data;
+//    });
+//    request.on('end', function () {
+//      var POST = qs.parse(body);
+//      var callback = function(err,stdout, stderr)
+//      {
+//        sys.puts(stdout); 
+//        response.writeHead(200, {'Content-Type': 'audio/mpeg'});
+//        response.write(fs.readFileSync(file));
+//        response.end();
+//        exec("rm " + file, function(){});
+//      };
+//      exec("echo 'this is a test' | text2wave | lame -V 1 - " + file, callback);
+//    });
+//  }
+//  else {
+//    request.on('end', function () {
+//      response.writeHead(200, {'Content-Type': 'text/html'});
+//      response.write(fs.readFileSync('index.html'));
+//      response.end();
+//    });
+//  }
+//}
+//).listen(1337);
 
 
 
