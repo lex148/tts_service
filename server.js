632cc974c7721196ff38e866a8d332b581dd9095
Lex Childs <lex148@gmail.com>
2011-07-03 09:46:19 -0500

    update to use express send

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var sys = require('sys');
 var exec = require('child_process').exec;
 var qs = require('querystring');
+var _und = require("underscore");
 
 function filename(){
   return "" + Math.floor(Math.random()*9999999) + ".mp3";
@@ -11,9 +12,18 @@ function filename(){
 
 function clean(str) {
   try{
-  return str.replace(/['"]/g,"");}
+    str = str.replace(/['";:]/g,"");
+    str = str.replace(/\.+/g,".");
+    return str;
+  }
   catch(err) {console.log(err); return "";}
 }
+
+function cleanup_file(file){
+    try{ exec("rm audio/" + file, function(){}); }
+    catch(err) {console.log(err); }
+}
+
 
 
 var app = require('express').createServer();
@@ -36,6 +46,7 @@ app.post('/read', function(request, response){
     var callback = function(err,stdout, stderr) {
       response.write("/audio/" + file);
       response.end();
+      _und.delay(function(){ cleanup_file(file); },20000);
     };
     exec("echo '" + clean(POST.text) + "' | text2wave | lame -V 1 - audio/" + file, callback);
   });
@@ -46,17 +57,19 @@ app.get('/audio/*', function(req, res){
   if( req.url.match(/audio\/(\w+\.\w+)/) ) {
     var file = req.url.match(/audio\/(\w+\.\w+)/)[1];
     try{
-      res.writeHead(200, {'Content-Type': 'audio/mpeg'});
-      res.write(fs.readFileSync('audio/' +  file));
-      //exec("rm audio/" + file, function(){});
+      fs.readFile('audio/' + file, function(err, data){
+        res.send(data, { 'Content-Type': 'audio/mpeg' }, 200 );
+      });
     }
-    catch(err) {console.log(err); }
+    catch(err) {
+      console.log(err); 
+      res.end();
+    }
   }
-  res.end();
 });
 
-app.listen(1337);
 
+app.listen(1337);
 
 
 console.log('Server running at http://127.0.0.1:1337/');
